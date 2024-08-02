58a59125a22704605092d47d3e32e565259dca32
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-02 14:58:04 +0700

    HW-01

---
+++ HW-01(try2).py
@@ -0,0 +1,18 @@
+def format_strings(*args):
+    if len(args) == 1 and ' ' in args[0]:
+        formatted_string = args[0].replace(' ', '-').upper()
+    else:
+        concatenated_string = ''
+        for arg in args:
+            concatenated_string += arg
+        formatted_string = concatenated_string.upper()
+    return formatted_string
+
+result = format_strings("Hello", "world", "this", "is", "a", "test")
+print(result)  # Output: "HELLOWORLDTHISISATEST"
+
+result = format_strings("Python", "is", "fun")
+print(result)  # Output: "PYTHONISFUN"
+
+result = format_strings("Hello world")
+print(result)  # Output: "HELLO-WORLD"
